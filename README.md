5197ed03612136ff8f552b174bc7a49cdff52d94
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-06-09 20:28:22 +0300

    Add new icon

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# [Shary](https://shary.in)
+# [shary.in](https://shary.in)
 
-Pastebin for images but much simpler and faster. After upload, you'll be redirected to a nice and short url which contains your image. Smooth and fast.
-
-https://shary.in
+Mobile pastebin for images but much simpler and faster. After upload, you'll be redirected to a nice and short url which contains your image. Smooth and fast.
---
+++ scripts/create-icons.sh
@@ -0,0 +1,11 @@
+cd ../app/
+convert image.png -resize 64x64 favicon.png
+convert image.png -resize 57x57 apple-touch-icon-57x57.png
+convert image.png -resize 114x114 apple-touch-icon-114x114.png
+convert image.png -resize 72x72 apple-touch-icon-72x72.png
+convert image.png -resize 144x144 apple-touch-icon-144x144.png
+convert image.png -resize 60x60 apple-touch-icon-60x60.png
+convert image.png -resize 120x120 apple-touch-icon-120x120.png
+convert image.png -resize 76x76 apple-touch-icon-76x76.png
+convert image.png -resize 152x152 apple-touch-icon-152x152.png
+convert image.png -resize 152x152 apple-touch-icon.png
